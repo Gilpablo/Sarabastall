0c816d3cb2c78077ce09299761bebf0d999be099
oscar <omc16@outlook.com>
2023-02-03 09:16:29 +0100

    todo junto

---
+++ Sarabastall1/App/config/configurar.php
@@ -0,0 +1,22 @@
+<?php
+
+//**Desarrollo */
+ini_set('display_errors',1);
+ini_set('display_starup_errors',1);
+error_reporting(E_ALL);
+//**Desarrollo */
+
+//Ruta de la aplicacion
+define('RUTA_APP', dirname(dirname(__FILE__)));
+
+//Ruta url, Ejemplo: http://localhost/atletismo
+define('RUTA_URL', 'http://localhost/sarabastal_prueba/Sarabastall1');
+
+define('NOMBRE_SITIO', 'Sarabastall');
+
+//configuracio de la base de datos
+
+define('DB_HOST', 'localhost');
+define('DB_USUARIO', 'root');
+define('DB_PASSWORD','root');
+define('DB_NOMBRE','infoargo');
